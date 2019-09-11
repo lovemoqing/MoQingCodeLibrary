038edef5bceb63fd68d2234fb353f1b3e7ee4afa
彭龙 <tccwpl@163.com>
2019-09-11 18:04:26 +0800

    .

---
+++ MoQing.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoQing.Domain;
+
+namespace MoQing.WebApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet, Route("/{id}")]
+        public ActionResult<ApiResult> Restart(string id)
+        {
+            return new ApiResult() { Code = 200, Msg = id, Data = null };
+        }
+    }
+}
