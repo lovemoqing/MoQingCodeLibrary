70849f8d3aee4cc3048d5a69164fb46cb3a6964d
unknown <pengl07@mysoft.com.cn>
2019-11-04 11:07:03 +0800

    添加JavascriptCommon

---
+++ Moqing.Webapp/wwwroot/JavaScript_Common/common.js
@@ -0,0 +1,25 @@
+﻿//JavascriptCommon
+var Common = function () {
+    var _this = this;
+    //获取当前的系统时间
+    _this.GetCurrSystemTime = function () {
+        function add(m) {
+            return m < 10 ? '0' + m : m
+        }
+        function fortime() {
+            var date = new Date();
+            var y = date.getFullYear();
+            var m = date.getMonth() + 1;
+            var d = date.getDate();
+            var h = date.getHours();
+            var mm = date.getMinutes();
+            var s = date.getSeconds();
+            return (y + '' + add(m) + '' + add(d) + '' + add(h) + '' + add(mm) + '' + add(s));
+        }
+        return fortime()
+    }
+}
+//实例化组件
+var common = new Common();
+//调用
+common.GetCurrSystemTime()
